2d87f2769d685bdb14d4293e1dcd85bb470f1212
IdDEv09 <146314456+IdDEv09@users.noreply.github.com>
2026-02-21 13:16:07 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ from payme import router as payme_router
 
 app = FastAPI()
 app.include_router(payme_router)
+@app.get("/")
+async def root():
+    return {"message": "Bot ishlayapti 🚀"}
 
 @app.post("/telegram/webhook")
 async def telegram_webhook(request: Request):
     data = await request.json()
     update = Update.model_validate(data)
     await dp.feed_update(bot, update)
+
     return {"ok": True}
